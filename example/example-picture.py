547ddaa2010a0880243cfc0f41f558f1ea7a2d8b
Yonatan Kaplonov <yonikas123@gmail.com>
2023-09-10 14:45:46 +0300

    added CNN explaenation and gave pictures a reasonable resolution

--- example/example-picture.py
+++ example/example-picture.py
@@ -3,4 +3,4 @@ from super_gradients.training import models
 yolo_nas_l = models.get("yolo_nas_l", pretrained_weights="coco")
 
 url = "Queen.webp"
-yolo_nas_l.predict(url, conf=0.25).show()
+yolo_nas_l.predict(url, conf=0.25).show() # this sets the conf to =0.25 meaning the confidence threshold is 0.25
--- example/example-video.py
+++ example/example-video.py
@@ -5,7 +5,8 @@ yolo_nas_l = models.get("yolo_nas_l", pretrained_weights="coco")
 
 input_video_path = "video.mp4"
 output_video_path = "detected.mp4"
-device = 'cuda' if torch.cuda.is_available() else "cpu"     #use gpu / cpu (will default to cpu if cuda is nota available)
-yolo_nas_l.to(device).predict(input_video_path).save(output_video_path)
+min_confidence_threshold = 0.6
+device = 'cuda' if torch.cuda.is_available() else "cpu"     #use gpu / cpu (will default to cpu if cuda is nota available) 
+yolo_nas_l.to(device).predict(input_video_path,conf=min_confidence_threshold).save(output_video_path)
 
 
