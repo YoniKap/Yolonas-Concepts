6e2df46d208766e8e25d93aa66126924c9e4c08f
Yonatan Kaplounov <yonikas123@gmail.com>
2024-01-25 22:32:21 +0200

    added cuda to example-picture

--- example/example-picture.py
+++ example/example-picture.py
@@ -1,6 +1,8 @@
 from super_gradients.training import models
+import torch
 
 yolo_nas_l = models.get("yolo_nas_l", pretrained_weights="coco")
 
 url = "Queen.webp"
-yolo_nas_l.predict(url, conf=0.6).show() # this sets the conf to =0.25 meaning the confidence threshold is 0.25
+device = 'cuda' if torch.cuda.is_available() else "cpu"     #use gpu / cpu (will default to cpu if cuda is nota available) 
+yolo_nas_l.to(device).predict(url, conf=0.2).show() # this sets the conf to =0.25 meaning the confidence threshold is 0.25
